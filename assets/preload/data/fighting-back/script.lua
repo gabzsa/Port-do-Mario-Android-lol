351b4df2101d55de4fd07aab80031617127a0b59
gabzsa <109770528+gabzsa@users.noreply.github.com>
2022-10-02 10:06:38 -0300

    Add files via upload

---
+++ assets/preload/data/fighting-back/script.lua
@@ -0,0 +1,8 @@
+function onCreatePost()
+
+	addScanlineEffect('camgame', 1.3)
+	addChromaticAbberationEffect('dad', 0.0003)
+	addChromaticAbberationEffect('bf', 0.0012)
+	addChromaticAbberationEffect('gf',0.0009)
+	addScanlineffect('camhud',true)
+end
